7eb5e93b6eb2341f9c37e5e1a556f56a3de86049
Karena Lai <65944717+klai890@users.noreply.github.com>
2022-12-27 16:39:15 -0500

    fixing build issue for auth

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -3,45 +3,45 @@
 /** ---------------------- AUTHORIZE -------------------------- */
 
 // This is an example of how to read a JSON Web Token from an API route
-import { getToken } from "next-auth/jwt"
+// import { getToken } from "next-auth/jwt"
 
-import type { NextApiRequest, NextApiResponse } from "next"
+// import type { NextApiRequest, NextApiResponse } from "next"
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  // If you don't have the NEXTAUTH_SECRET environment variable set,
-  // you will have to pass your secret as `secret` to `getToken`
-  const token = await getToken({ req })
-  res.send(JSON.stringify(token, null, 2))
-}
+// export default async function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse
+// ) {
+//   // If you don't have the NEXTAUTH_SECRET environment variable set,
+//   // you will have to pass your secret as `secret` to `getToken`
+//   const token = await getToken({ req })
+//   res.send(JSON.stringify(token, null, 2))
+// }
 
 /**  --------------------- CALL STRAVA API -------------------- */
 
-// var StravaApiV3 = require('strava_api_v3');
-// var defaultClient = StravaApiV3.ApiClient.instance;
+var StravaApiV3 = require('strava_api_v3');
+var defaultClient = StravaApiV3.ApiClient.instance;
 
 // Configure OAuth2 access token for authorization: strava_oauth
-// var strava_oauth = defaultClient.authentications['strava_oauth'];
-// strava_oauth.accessToken = "3ae0997262becbed5cd70ac8ac2f26c00eae7a78"
+var strava_oauth = defaultClient.authentications['strava_oauth'];
+strava_oauth.accessToken = "3ae0997262becbed5cd70ac8ac2f26c00eae7a78"
 
-// var api = new StravaApiV3.AthletesApi()
+var api = new StravaApiV3.AthletesApi()
 
 
 /* ----------------------- MAKE STRAVA API CALL --------------------- */
-// export default function handler(req, res) {
-//   var callback = function(error, data, response) {
-//     if (error) {
-//       console.error(error);
-//     } else {
-//       console.log('API called successfully. Returned data: ' + data);
-//     }
-    // console.log("<---------------------RESPONSE------------------->");
-    // console.log(JSON.parse(response.text)); // oh god, please remember to do JSON.parse....
+export default function handler(req, res) {
+  var callback = function(error, data, response) {
+    if (error) {
+      console.error(error);
+    } else {
+      console.log('API called successfully. Returned data: ' + data);
+    }
+    console.log("<---------------------RESPONSE------------------->");
+    console.log(JSON.parse(response.text)); // oh god, please remember to do JSON.parse....
 
-//     res.status(200).json({ data: response.text })
-//   };
+    res.status(200).json({ data: response.text })
+  };
 
-//   api.getLoggedInAthlete(callback);
-// };
+  api.getLoggedInAthlete(callback);
+};
