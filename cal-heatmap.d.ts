4d25c42fdab7dd9dc5be12d9daa25f699e35ae99
Karena Lai <karena_lai@ucsb.edu>
2025-09-06 14:20:48 -0400

    Fix build script error by adding typescript definition for cal-heatmap, adding @vitejs/plugin-react-swc to fix issue in vitest.config.ts

---
+++ cal-heatmap.d.ts
@@ -0,0 +1,14 @@
+declare module "cal-heatmap" {
+  const CalHeatmap: any;
+  export default CalHeatmap;
+}
+
+declare module "cal-heatmap/plugins/CalendarLabel" {
+  const CalendarLabel: any;
+  export default CalendarLabel;
+}
+
+declare module "cal-heatmap/plugins/Tooltip" {
+  const Tooltip: any;
+  export default Tooltip;
+}
